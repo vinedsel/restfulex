59daac00976306e865a5829f7ae7d87db2723196
Raymond Gregory <vinedsel@gmail.com>
2018-04-19 05:20:58 -0500

    Updated all dependencies in index.js file.

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ const handlebars = require("express-handlebars").create({
   defaultLayout: 'main'
 });
 
+const sequelize = new Sequelize('database', 'username', null, {
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: "./Chinook_Sqlite_AutoIncrementPKs.sqlite"
+});
+
+const app = express();
+
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
+
+app.use(require("body-parser")());
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
