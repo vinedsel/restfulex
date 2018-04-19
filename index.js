7cf6176c781d1e62238397796b49cf261a692d2d
Raymond Gregory <vinedsel@gmail.com>
2018-04-19 05:12:52 -0500

    Installed Express-sqlite3 modules.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const express = require('express');
+const handlebars = require("express-handlebars").create({
+  defaultLayout: 'main'
+});
+
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
