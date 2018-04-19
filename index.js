035f8c9fb6c543e93e7cfb6ead3870e3b1fa4425
Raymond Gregory <vinedsel@gmail.com>
2018-04-19 16:15:31 -0500

    Modified these files.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require('express');
 const Sequelize = require("Sequelize");
 const sqlite3 = require('sqlite3').verbose();
@@ -13,8 +14,6 @@ const sequelize = new Sequelize('database', 'username', null, {
 
 const app = express();
 
-const cors = require("cors");
-
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
 
 app.use(require("body-parser")());
@@ -22,7 +21,51 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
+// CODE BLOCKS
 
+const Artist = sequelize.define(
+  "Artist", {
+    ArtistId: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Name: Sequelize.STRING
+  }, {
+    freezeTableName: true,
+    timestamps: false
+  });
+
+const Album = sequelize.define(
+  "Album", {
+    ArtistId: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Title: Sequelize.STRING
+  }, {
+    freezeTableName: true,
+    timestamps: false
+  });
+
+  Artist.hasMany(Album, {foreignKey: 'ArtistId'});
+  Album.belongsTo(Artist, {foreignKey: 'ArtistId'});
+    app.get('/', (req, res) => {
+      Album.findAll({
+          include: [
+              {
+                  model: Artist
+              }
+          ]
+      }).then(albums => {
+          res.render('view', {results: albums})
+      });
+  });
+
+app.get('/view', cors(), function (req, res) {
+  res.send("This route is CORS enabled!");
+});
 
 
 app.listen(process.env.PORT || 3000, () => {
